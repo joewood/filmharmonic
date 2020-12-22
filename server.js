ebb5ed8e5111ce6a2a5fc543dd15d7bb958a3a22
Joe Wood <joewood@live.com>
2020-12-21 20:34:07 -0500

    check in

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
+app.get('/hi', function (req, res) {
   res.send('Hello World');
 });
 
-var server = app.listen(8081, function () {
+var server = app.listen(process.env.PORT || 8081, function () {
   var host = server.address().address;
   var port = server.address().port;
 
