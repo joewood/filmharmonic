eaf8c494bea8cb94b9c0ac0755ee6b2c13234312
Joe Wood <joewood@live.com>
2020-12-21 23:24:57 -0500

    Server

--- server.js
+++ server.js
@@ -2,29 +2,25 @@ const express = require('express');
 const app = express();
 const fetch = require('node-fetch');
 
-app.get('/api/hi', function (req, res) {
-  res.send('Hello World');
-});
-
 app.get('/api/search/:search', async function (req, res) {
   const search = req.params.search;
-  const r = await fetch(`http://www.omdbapi.com/?apikey=d88baf32&s=${search}`, { method: 'GET' });
-  if (r.ok) {
-    const oo = await r.json();
-    res.send(JSON.stringify(oo, null, 2));
+  const omdbRequest = await fetch(`http://www.omdbapi.com/?apikey=d88baf32&s=${search}`, { method: 'GET' });
+  if (omdbRequest.ok) {
+    const data = await omdbRequest.json();
+    res.send(JSON.stringify(data, null, 2));
     res.status(200);
   } else {
-    console.error(r.statusText);
-    res.status(r.status || 501);
-    res.send(r.statusText);
+    console.error(omdbRequest.statusText);
+    res.status(omdbRequest.status || 501);
+    res.send(omdbRequest.statusText);
   }
 });
 
 app.use(express.static('public'));
 
-var server = app.listen(process.env.PORT || 8081, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+const server = app.listen(process.env.PORT || 8081, function () {
+  const host = server.address().address;
+  const port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
