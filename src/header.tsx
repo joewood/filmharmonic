3e1134fe37e6dcb16fe22281c77864fa46bebc7d
Joe Wood <joewood@live.com>
2021-01-04 21:16:53 -0500

    search in header

--- src/header.tsx
+++ src/header.tsx
@@ -1,13 +1,47 @@
+import { navigate, useLocation } from '@reach/router';
 import { User } from 'oidc-client';
+import { parse } from 'query-string';
 import * as React from 'react';
-import { FC } from 'react';
+import { FC, FormEventHandler, useRef } from 'react';
 
-export const Header: FC<{ user: User | null }> = ({ user }) => (
-  <header className="App-header">
-    <h1 className= "header-object">Film Harmonic</h1>
-    <a className="user header-object" href="/user">
-      {user?.profile.name}
-    </a>
-    <img className= "header-object" width={100} src={user?.profile.picture} alt="profile" />
-  </header>
-);
+export const Header: FC<{ user: User | null }> = ({ user }) => {
+  // in React the `useRef` function gives us a ref object that can be used to link to an HTML Element
+  // this ref will link to the Search Input element below
+  const searchRefInputElement = useRef<HTMLInputElement>(null);
+  const location = useLocation();
+  const { search } = parse(location.search) as { search: string };
+  // When the user presses "Submit" we'll run the `onSubmit` function. This function won't submit
+  // any values to the server. Instead it uses the value of the Search Input box to call the function
+  // `searchMovies`. The value of the input bo is in the `.current.value` of the ref.
+  const onSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
+    // preventDefault - stop the Form posting to the server
+    event.preventDefault();
+    navigate(`/search?search=${searchRefInputElement.current?.value || ''}`);
+  };
+
+  return (
+    <header className="App-header">
+      <h1 className="header-object">Film Harmonic</h1>
+      <a className="user header-object" href="/user">
+        {user?.profile.name}
+      </a>
+
+      <img className="header-object" width={100} src={user?.profile.picture} alt="profile" />
+
+      <div className="search">
+        <form onSubmit={onSubmit} method="POST">
+          <input
+            className="searchbar"
+            placeholder="Search.."
+            ref={searchRefInputElement}
+            name="search"
+            defaultValue={search}
+            type="text"
+            spellCheck={false}
+          />
+          <input type="submit" value="🎥" />
+        </form>
+      </div>
+    </header>
+  );
+};
--- src/search.tsx
+++ src/search.tsx
@@ -13,10 +13,6 @@ interface SearchProps extends RouteComponentProps {
 
 /** In React a Function is like an HTML element, this is the <App> Component used in index.tsx */
 export const Search: FC<SearchProps> = ({ user }) => {
-  // in React the `useRef` function gives us a ref object that can be used to link to an HTML Element
-  // this ref will link to the Search Input element below
-  const searchRefInputElement = useRef<HTMLInputElement>(null);
-
   const location = useLocation();
   const { search } = parse(location.search) as { search: string };
 
@@ -25,15 +21,6 @@ export const Search: FC<SearchProps> = ({ user }) => {
   // the first element is the current state value, the second element in the array is the function to
   // change that value. We're using state to keep the list of Movies returned by the search
   const [movies, setMovies] = useState<Movie[]>([]);
-
-  // When the user presses "Submit" we'll run the `onSubmit` function. This function won't submit
-  // any values to the server. Instead it uses the value of the Search Input box to call the function
-  // `searchMovies`. The value of the input bo is in the `.current.value` of the ref.
-  const onSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
-    // preventDefault - stop the Form posting to the server
-    event.preventDefault();
-    navigate(`/search?search=${searchRefInputElement.current?.value || ''}`);
-  };
 
   useEffect(() => {
     // if the ref is null clear the movie list
@@ -47,26 +34,17 @@ export const Search: FC<SearchProps> = ({ user }) => {
   return (
     <div className="App">
       <Header user={user} />
-      <div className="search">
-        <form onSubmit={onSubmit} method="POST">
-          <label>
-            Search
-            <input className="searchbar" placeholder="Search.." ref={searchRefInputElement} name="search" type="text" spellCheck={false} />
-          </label>
-          <input type="submit" value="🎥" />
-        </form>
-      </div>
       <div className="con">
-      <ul>
-        {movies.map((movie) => (
-          <div className="movie" key={movie.Title}>
-            <a href={`/movie/${movie.imdbID}`}>
-              <img src={movie.Poster} alt={movie.Title} width={100}></img>
-              <p>{movie.Title}</p>
-            </a>
-          </div>
-        ))}
-      </ul>
+        <ul>
+          {movies.map((movie) => (
+            <div className="movie" key={movie.Title}>
+              <a href={`/movie/${movie.imdbID}`}>
+                <img src={movie.Poster} alt={movie.Title} width={100}></img>
+                <p>{movie.Title}</p>
+              </a>
+            </div>
+          ))}
+        </ul>
       </div>
     </div>
   );
