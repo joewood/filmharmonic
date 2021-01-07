3a2f45b286159121c55bbc5c65adf18ca52d0cef
Sophia Wood <sophiaew@live.com>
2021-01-07 15:02:31 -0500

    changes

--- src/header.tsx
+++ src/header.tsx
@@ -1,8 +1,9 @@
-import { navigate, useLocation } from '@reach/router';
-import { User } from 'oidc-client';
-import { parse } from 'query-string';
-import * as React from 'react';
-import { FC, FormEventHandler, useRef } from 'react';
+import { navigate, useLocation } from "@reach/router";
+import { User } from "oidc-client";
+import { parse } from "query-string";
+import * as React from "react";
+import { FC, FormEventHandler, useRef } from "react";
+import logo from "./images/Logo.png";
 
 export const Header: FC<{ user: User | null }> = ({ user }) => {
   // in React the `useRef` function gives us a ref object that can be used to link to an HTML Element
@@ -16,19 +17,20 @@ export const Header: FC<{ user: User | null }> = ({ user }) => {
   const onSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
     // preventDefault - stop the Form posting to the server
     event.preventDefault();
-    navigate(`/search?search=${searchRefInputElement.current?.value || ''}`);
+    navigate(`/search?search=${searchRefInputElement.current?.value || ""}`);
   };
-
+  console.log(logo);
   return (
     <header className="App-header">
-      <h1 className="header-object">Film Harmonic</h1>
-      <a className="user header-object" href="/user">
-        {user?.profile.name}
-      </a>
+      <img className="header-object logo" alt="logo" src={logo} />
+      <div className="user">
+        <img className="header-object user-image" height={"32"} src={user?.profile.picture} alt="profile" />
+        <a className="user header-object" href="/user">
+          {user?.profile.name}
+        </a>
+      </div>
 
-      <img className="header-object" width={100} src={user?.profile.picture} alt="profile" />
-
-      <div className="search">
+      <div className="search header-object">
         <form onSubmit={onSubmit} method="POST">
           <input
             className="searchbar"
